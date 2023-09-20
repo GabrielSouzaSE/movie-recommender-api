2e70a9bf690051f76893fd44b8b2ecc344bdcec1
Gabriel Souza <gabrielsouzase@gmail.com>
2023-09-20 11:08:50 -0300

    Update mygenetic.py

--- ga/mygenetic.py
+++ ga/mygenetic.py
@@ -1,5 +1,3 @@
-
-
 from ga.algorithm import Algorithm
 from sqlalchemy.orm import Session
 from fastapi import Depends
@@ -29,8 +27,7 @@ class MyGeneticAlgorithm(Algorithm):
         self.all_ids = all_ids
         self.query_search = query_search
         
-
-    # # Do Professor
+    # # DO PROFESSOR
     # def evaluate(self, individual):
 
     #     if len(individual) != len(set(individual)):
@@ -48,6 +45,7 @@ class MyGeneticAlgorithm(Algorithm):
 
     #     return (mean_, )
 
+    #Código DO CLAUDIO
     def evaluate(self, individual):
         # Verifique se o indivíduo possui IDs únicos e pertence à lista de IDs válidos
         if len(individual) != len(set(individual)) or any(id not in self.all_ids for id in individual):
@@ -73,3 +71,60 @@ class MyGeneticAlgorithm(Algorithm):
             # Se nenhum filme for encontrado, retorne aptidão zero
             return (0.0,)
 
+    # # TESTE 01 -  TENTANDO UTILIZAR O USUÁRIO, O QUAL VAI ANALISAR
+    # def evaluate(self, individual):
+    #     if len(individual) != len(set(individual)):
+    #         return (0.0, )
+
+    #     if len(list(set(individual) - set(self.all_ids))) > 0:
+    #         return (0.0, )
+
+    #     # Obtenha as avaliações do usuário identificado por self.query_search
+    #     user_ratings = RatingsRepository.find_by_userid(self.db, self.query_search)
+
+    #     # Extraia os IDs dos filmes avaliados pelo usuário
+    #     user_movie_ids = [rating.movieId for rating in user_ratings]
+
+    #     # Verifique se os filmes recomendados estão na lista de filmes avaliados pelo usuário
+    #     recommended_movie_ids = set(individual)
+    #     intersection = set(user_movie_ids).intersection(recommended_movie_ids)
+
+    #     # Calcule a média das avaliações dos filmes recomendados pelo usuário
+    #     if len(intersection) > 0:
+    #         ratings_movies = RatingsRepository.find_by_movieid_list(self.db, list(intersection))
+    #         mean_rating = np.mean([rating.rating for rating in ratings_movies])
+    #     else:
+    #         mean_rating = 0.0
+
+    #     return (mean_rating, )
+
+    # # TESTE 02(COM O CóDIGO DO CLAUDIO) -  TENTANDO UTILIZAR O USUÁRIO, O QUAL VAI ANALISAR
+    # def evaluate(self, individual):
+    #     # Verifique se o indivíduo possui IDs únicos e pertence à lista de IDs válidos
+    #     if len(individual) != len(set(individual)) or any(id not in self.all_ids for id in individual):
+    #         return (0.0,)
+
+    #     # Obtenha as avaliações do usuário identificado por self.query_search
+    #     user_ratings = RatingsRepository.find_by_userid(self.db, self.query_search)
+
+    #     if len(user_ratings) == 0:
+    #         return (0.0,)
+
+    #     # Encontre as classificações dos filmes representados pelo indivíduo no banco de dados
+    #     ratings_movies = RatingsRepository.find_by_movieid_list(self.db, individual)
+
+    #     if len(ratings_movies) > 0:
+    #         # Calcule a média das classificações dos filmes representados pelo indivíduo
+    #         mean_rating = np.mean([obj.rating for obj in ratings_movies])
+
+    #         # Calcule a variância das classificações dos filmes representados pelo indivíduo
+    #         variance = np.var([obj.rating for obj in ratings_movies])
+
+    #         # Calcule a métrica de aptidão ponderada (por exemplo, média - 0.2 * variância)
+    #         fitness = mean_rating - 0.2 * variance
+
+    #         # Retorne a métrica de aptidão em uma tupla
+    #         return (fitness,)
+    #     else:
+    #         # Se nenhum filme for encontrado, retorne aptidão zero
+    #         return (0.0,)
